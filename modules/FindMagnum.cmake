181e907e81e7cca37128cd76b5dc0ec6f9827f80
Vladimír Vondruš <mosra@centrum.cz>
2011-12-09 15:27:15 +0100

    Updated FindMagnum.cmake from Magnum repository.

--- modules/FindMagnum.cmake
+++ modules/FindMagnum.cmake
@@ -2,9 +2,10 @@
 #
 # This module depends on Corrade and additionally defines:
 #
-# MAGNUM_FOUND          - True if Magnum library is found
-# MAGNUM_INCLUDE_DIR    - Include dir for Magnum
-# MAGNUM_LIBRARY        - Magnum library
+# MAGNUM_FOUND                      - True if Magnum library is found
+# MAGNUM_INCLUDE_DIR                - Include dir for Magnum
+# MAGNUM_LIBRARY                    - Magnum library
+# MAGNUM_PRIMITIVES_LIBRARY         - Library with primitives
 #
 # MAGNUM_LIBRARY_INSTALL_DIR        - Library installation directory
 # MAGNUM_CMAKE_MODULE_INSTALL_DIR   - Installation dir for CMake modules
@@ -12,8 +13,10 @@
 #
 
 find_package(Corrade REQUIRED)
+find_package(OpenGL REQUIRED)
+find_package(GLEW REQUIRED)
 
-if (MAGNUM_INCLUDE_DIR AND MAGNUM_LIBRARY)
+if (MAGNUM_INCLUDE_DIR AND MAGNUM_LIBRARY AND MAGNUM_PRIMITIVES_LIBRARY)
 
     # Already in cache
     set(MAGNUM_FOUND TRUE)
@@ -21,6 +24,7 @@ if (MAGNUM_INCLUDE_DIR AND MAGNUM_LIBRARY)
 else()
     # Libraries
     find_library(MAGNUM_LIBRARY Magnum)
+    find_library(MAGNUM_PRIMITIVES_LIBRARY MagnumPrimitives)
 
     # Paths
     find_path(MAGNUM_INCLUDE_DIR
@@ -32,6 +36,7 @@ else()
     find_package_handle_standard_args("Magnum" DEFAULT_MSG
         MAGNUM_INCLUDE_DIR
         MAGNUM_LIBRARY
+        MAGNUM_PRIMITIVES_LIBRARY
     )
 
 endif()
@@ -39,6 +44,7 @@ endif()
 # Installation dirs
 set_parent_scope(MAGNUM_LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib)
 set_parent_scope(MAGNUM_PLUGINS_INSTALL_DIR ${MAGNUM_LIBRARY_INSTALL_DIR}/magnum)
+set_parent_scope(MAGNUM_PLUGINS_IMPORTER_INSTALL_DIR ${MAGNUM_PLUGINS_INSTALL_DIR}/importers)
 set_parent_scope(MAGNUM_CMAKE_MODULE_INSTALL_DIR ${CMAKE_ROOT}/Modules)
 set_parent_scope(MAGNUM_INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/include/Magnum)
 set_parent_scope(MAGNUM_PLUGINS_INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/include/Magnum/Plugins)
