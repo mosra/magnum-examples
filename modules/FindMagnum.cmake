59fee058d4db46b9958bcd4d9884bdf591ffa703
Vladimír Vondruš <mosra@centrum.cz>
2012-08-03 00:29:17 +0200

    Updated FindCorrade.cmake and FindMagnum.cmake.

--- modules/FindMagnum.cmake
+++ modules/FindMagnum.cmake
@@ -1,55 +1,51 @@
 # - Find Magnum
 #
 # Basic usage:
-#
 #  find_package(Magnum [REQUIRED])
-#
 # This command tries to find Magnum library and then defines:
-#
 #  MAGNUM_FOUND                 - Whether the library was found
-#  MAGNUM_TARGET_GLES           - Defined if Magnum was built for OpenGL ES,
-#   slightly reducing feature count. The same variable is also #defined in
-#   Magnum headers.
+#  MAGNUM_TARGET_GLES           - Defined if Magnum was built for OpenGL
+#   ES, slightly reducing feature count. The same variable is also
+#   #defined in Magnum headers.
 #  MAGNUM_LIBRARIES             - Magnum library and dependent libraries
 #  MAGNUM_INCLUDE_DIRS          - Root include dir and include dirs of
 #   dependencies
 #  MAGNUM_PLUGINS_IMPORTER_DIR  - Directory with importer plugins
-#
 # This command will try to find only the base library, not the optional
-# components. The base library depends on Corrade, OpenGL and GLEW libraries.
-# Additional dependencies are specified by the components. The optional
-# components are:
-#
+# components. The base library depends on Corrade, OpenGL and GLEW
+# libraries. Additional dependencies are specified by the components. The
+# optional components are:
 #  MeshTools     - MeshTools library
 #  Physics       - Physics library
 #  Primitives    - Library with stock geometric primitives (static)
 #  Shaders       - Library with stock shaders
-#
 #  EglContext    - EGL context (depends on EGL and X11 libraries)
 #  GlutContext   - GLUT context (depends on GLUT library)
 #  Sdl2Context   - SDL2 context (depends on SDL2 library)
-#
 # Example usage with specifying additional components is:
-#
-#  find_package(Magnum [REQUIRED|COMPONENTS] MeshTools Primitives GlutContext)
-#
+#  find_package(Magnum [REQUIRED|COMPONENTS]
+#               MeshTools Primitives GlutContext)
 # For each component is then defined:
-#
 #  MAGNUM_*_FOUND   - Whether the component was found
 #  MAGNUM_*_LIBRARIES - Component library and dependent libraries
+#  MAGNUM_*_INCLUDE_DIRS - Include dirs of module dependencies
 #
 # Additionally these variables are defined for internal usage:
-#
-#  MAGNUM_INCLUDE_DIR                   - Root include dir (w/o dependencies)
-#  MAGNUM_LIBRARY                       - Magnum library (w/o dependencies)
-#  MAGNUM_*_LIBRARY                     - Component libraries (w/o dependencies)
-#
+#  MAGNUM_INCLUDE_DIR                   - Root include dir (w/o
+#   dependencies)
+#  MAGNUM_LIBRARY                       - Magnum library (w/o
+#   dependencies)
+#  MAGNUM_*_LIBRARY                     - Component libraries (w/o
+#   dependencies)
 #  MAGNUM_LIBRARY_INSTALL_DIR           - Library installation directory
 #  MAGNUM_PLUGINS_INSTALL_DIR           - Plugin installation directory
-#  MAGNUM_PLUGINS_IMPORTER_INSTALL_DIR  - Importer plugin installation directory
-#  MAGNUM_CMAKE_MODULE_INSTALL_DIR      - Installation dir for CMake modules
+#  MAGNUM_PLUGINS_IMPORTER_INSTALL_DIR  - Importer plugin installation
+#   directory
+#  MAGNUM_CMAKE_MODULE_INSTALL_DIR      - Installation dir for CMake
+#   modules
 #  MAGNUM_INCLUDE_INSTALL_DIR           - Header installation directory
-#  MAGNUM_PLUGINS_INCLUDE_INSTALL_DIR   - Plugin header installation directory
+#  MAGNUM_PLUGINS_INCLUDE_INSTALL_DIR   - Plugin header installation
+#   directory
 #
 
 # Dependencies
@@ -108,6 +104,7 @@ foreach(component ${Magnum_FIND_COMPONENTS})
             find_package(SDL2)
             if(SDL2_FOUND)
                 set(_MAGNUM_${_COMPONENT}_LIBRARIES ${SDL2_LIBRARY})
+                set(_MAGNUM_${_COMPONENT}_INCLUDE_DIRS ${SDL2_INCLUDE_DIR})
             else()
                 unset(MAGNUM_${_COMPONENT}_LIBRARY)
             endif()
@@ -155,6 +152,7 @@ foreach(component ${Magnum_FIND_COMPONENTS})
     # Decide if the library was found
     if(MAGNUM_${_COMPONENT}_LIBRARY AND _MAGNUM_${_COMPONENT}_INCLUDE_DIR)
         set(MAGNUM_${_COMPONENT}_LIBRARIES ${MAGNUM_${_COMPONENT}_LIBRARY} ${_MAGNUM_${_COMPONENT}_LIBRARIES})
+        set(MAGNUM_${_COMPONENT}_INCLUDE_DIRS ${_MAGNUM_${_COMPONENT}_INCLUDE_DIRS})
         set(Magnum_${component}_FOUND TRUE)
     else()
         set(Magnum_${component}_FOUND FALSE)
