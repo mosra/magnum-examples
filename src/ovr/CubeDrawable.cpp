4d8d26f3a8617ab21298837d5575c04e96268d4c
Squareys <Squareys@googlemail.com>
2015-06-03 19:48:23 +0200

    ovr: Add CubeDrawable.

---
+++ src/ovr/CubeDrawable.cpp
@@ -0,0 +1,59 @@
+/*
+    This file is part of Magnum.
+
+    Copyright © 2010, 2011, 2012, 2013, 2014, 2015
+              Vladimír Vondruš <mosra@centrum.cz>
+    Copyright © 2015
+              Jonathan Hale <squareys@googlemail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "CubeDrawable.h"
+
+#include <Magnum/Mesh.h>
+#include <Magnum/SceneGraph/Camera3D.h>
+#include <Magnum/Shaders/Phong.h>
+
+namespace Magnum { namespace Examples {
+
+CubeDrawable::CubeDrawable(Mesh* mesh, Shaders::Phong* shader, const Vector3& color, Object3D* parent, SceneGraph::DrawableGroup3D* group): Object3D(parent), SceneGraph::Drawable3D(*this, group), _mesh(mesh), _shader(shader), _color(color) {
+}
+
+void CubeDrawable::draw(const Matrix4& transformationMatrix, SceneGraph::AbstractCamera3D& camera) {
+    _shader->setDiffuseColor(_color)
+        .setSpecularColor(Color3(1.0f))
+        .setShininess(20)
+        .setLightPosition(camera.cameraMatrix().transformPoint({3.0f, 3.0f, 3.0f}))
+        .setTransformationMatrix(transformationMatrix)
+        .setNormalMatrix(transformationMatrix.rotationScaling())
+        .setProjectionMatrix(camera.projectionMatrix());
+
+    _mesh->draw(*_shader);
+}
+
+void CubeDrawable::setColor(Color3 color) {
+    _color = color;
+}
+
+Color3 CubeDrawable::getColor(void) {
+    return _color;
+}
+
+}}
