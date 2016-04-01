856a465d80cceaf4cc715d4540b9ef7a6fd84d8f
Squareys <Squareys@googlemail.com>
2016-04-01 17:01:35 +0200

    modules: Update FindOVR.cmake
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- modules/FindOVR.cmake
+++ modules/FindOVR.cmake
@@ -18,7 +18,7 @@
 #
 #   Copyright © 2010, 2011, 2012, 2013, 2014, 2015, 2016
 #             Vladimír Vondruš <mosra@centrum.cz>
-#   Copyright © 2015 Jonathan Hale <squareys@googlemail.com>
+#   Copyright © 2015, 2016 Jonathan Hale <squareys@googlemail.com>
 #
 #   Permission is hereby granted, free of charge, to any person obtaining a
 #   copy of this software and associated documentation files (the "Software"),
@@ -39,28 +39,59 @@
 #   DEALINGS IN THE SOFTWARE.
 #
 
-set(LIBOVR_ROOT ${CMAKE_INSTALL_PREFIX})
-
 if(NOT OVR_SDK_ROOT)
-    message(WARNING "OVR_SDK_ROOT is not set. Will try to find headers and library in ${CMAKE_INSTALL_PREFIX}." )
-else()
-    set(LIBOVR_ROOT ${OVR_SDK_ROOT}/LibOVR)
+    find_path(OVR_SDK_ROOT OculusSDK)
+
+    if(OVR_SDK_ROOT)
+        set(OVR_SDK_ROOT "${OVR_SDK_ROOT}/OculusSDK")
+        message(STATUS "Found OculusSDK: ${OVR_SDK_ROOT}")
+    endif()
+
 endif()
+
+set(LIBOVR_ROOT ${OVR_SDK_ROOT}/LibOVR)
 
 # find include directory
 find_path(OVR_INCLUDE_DIR NAMES OVR_CAPI.h HINTS ${LIBOVR_ROOT}/Include)
 
 if(WIN32)
     if(MSVC)
-        find_library(OVR_LIBRARY NAMES LibOVR HINTS ${LIBOVR_ROOT}/Lib/Windows)
+        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+            # compiling for 64 bit
+            set(_OVR_MSVC_ARCH "x64")
+        else()
+            # compiling for 32 bit
+            set(_OVR_MSVC_ARCH "Win32")
+        endif()
+
+        # select the correct library folder matching MSVC version
+        if (MSVC_VERSION GREATER 1800)
+            set(_OVR_MSVC_NAME "VS2015")
+        elseif (MSVC_VERSION GREATER 1700)
+            set(_OVR_MSVC_NAME "VS2013")
+        elseif (MSVC_VERSION GREATER 1600)
+            set(_OVR_MSVC_NAME "VS2012")
+        else()
+            set(_OVR_MSVC_NAME "VS2010")
+        endif()
+
+        find_library(OVR_LIBRARY NAMES LibOVR HINTS "${LIBOVR_ROOT}/Lib/Windows/${_OVR_MSVC_ARCH}/Release/${_OVR_MSVC_NAME}")
+
+        unset(_OVR_MSVC_ARCH)
+        unset(_OVR_MSVC_NAME)
     elseif(MINGW)
-        # linking against the MSVC dll with MinGW does not work directly. Instead, you need to
-        # link against a specific version. This will cause problems with newer oculus runtimes,
-        # though. (FIXME!)
-        # The clean way to link against libOVR, which seems to require the Windows DDK
-        find_library(OVR_LIBRARY NAMES LibOVRRT32_0_8.dll HINTS "C:/Windows/SysWOW64")
-        #find_library(OVR_LIBRARY NAMES LibOVR HINTS ${LIBOVR_ROOT}/Lib/Windows/Win32/Release/VS2012)
+        # we cannot link against the MSVC lib with MinGW. Instead, we link directly to the runtime DLL,
+        # which requires the Oculus runtime to be installed.
+        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+            # compiling for 64 bit
+            find_library(OVR_LIBRARY NAMES LibOVRRT64_1.dll HINTS "C:/Program Files (x86)/Oculus/Support/oculus-runtime")
+        else()
+            # compiling for 32 bit
+            find_library(OVR_LIBRARY NAMES LibOVRRT32_1.dll HINTS "C:/Program Files (x86)/Oculus/Support/oculus-runtime")
+        endif()
     endif()
+else()
+    error("The Oculus SDK does not support ${CMAKE_SYSTEM_NAME}.")
 endif()
 
 include(FindPackageHandleStandardArgs)
