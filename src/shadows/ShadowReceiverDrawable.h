590b213f0785c79241d8a50749b2efadaf3994d1
Vladimír Vondruš <mosra@centrum.cz>
2016-10-01 13:10:22 +0200

    shadows: added explicit public domain / unlicense block.

--- src/shadows/ShadowReceiverDrawable.h
+++ src/shadows/ShadowReceiverDrawable.h
@@ -1,13 +1,33 @@
-/* magnum-shadows - A Cascading/Parallel-Split Shadow Mapping example
- * Written in 2016 by Bill Robinson airbaggins@gmail.com
- *
- * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to
- * this software to the public domain worldwide. This software is distributed without any warranty.
- *
- * See <http://creativecommons.org/publicdomain/zero/1.0/>.
- *
- * Credit is appreciated, but not required.
- * */
+/*
+    This file is part of Magnum.
+
+    Original authors — credit is appreciated but not required:
+
+        2010, 2011, 2012, 2013, 2014, 2015, 2016 —
+            Vladimír Vondruš <mosra@centrum.cz>
+        2016 — Bill Robinson <airbaggins@gmail.com>
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+    this software, either in source code form or as a compiled binary, for any
+    purpose, commercial or non-commercial, and by any means.
+
+    In jurisdictions that recognize copyright laws, the author or authors of
+    this software dedicate any and all copyright interest in the software to
+    the public domain. We make this dedication for the benefit of the public
+    at large and to the detriment of our heirs and successors. We intend this
+    dedication to be an overt act of relinquishment in perpetuity of all
+    present and future rights to this software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #pragma once
 #include <Magnum/SceneGraph/Drawable.h>
 #include <Magnum/Mesh.h>
