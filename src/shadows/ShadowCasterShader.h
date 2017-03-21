0b2f3181d3f55038732d1bd36142cf0513bb90f1
Vladimír Vondruš <mosra@centrum.cz>
2017-03-21 02:21:25 +0100

    Updated copyright year.

--- src/shadows/ShadowCasterShader.h
+++ src/shadows/ShadowCasterShader.h
@@ -5,7 +5,7 @@
 
     Original authors — credit is appreciated but not required:
 
-        2010, 2011, 2012, 2013, 2014, 2015, 2016 —
+        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 —
             Vladimír Vondruš <mosra@centrum.cz>
         2016 — Bill Robinson <airbaggins@gmail.com>
 
