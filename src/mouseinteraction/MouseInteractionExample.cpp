a36ddc39fd9a40007c8b8ca8d83a7618e3c8c0df
Vladimír Vondruš <mosra@centrum.cz>
2018-08-25 23:39:43 +0200

    mouseinteraction: simplification and coding style updates.
    What I've done:
    
     * There's just one application class to make the juicy parts stand out
       more. Should make it easier to tear apart and reuse.
     * Vectorized most of the math to be consistent with how things are
       usually done in Magnum (so no separate x/y/z components)
     * Fixed the depth averaging (was using pixelSize(), which is *not* the
       amount of pixels in an image)
     * Removed use of the Magnum:: prefix, using Magnum typedefs for scalars
     * Clarified symbol names, converted names from snail_case to camelCase
     * The non-camera-relative unprojection was used nowhere in the code, so
       I turned it into just a comment
     * Converted the mouse controls to resemble Blender more (muscle memory
       is hard to beat, sorry :D)

--- src/mouseinteraction/MouseInteractionExample.cpp
+++ src/mouseinteraction/MouseInteractionExample.cpp
@@ -28,14 +28,11 @@
     CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include <algorithm>
-
+#include <Magnum/Image.h>
 #include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/Renderer.h>
-#include <Magnum/Image.h>
-#include <Magnum/Magnum.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/SceneGraph/Camera.h>
 #include <Magnum/SceneGraph/Drawable.h>
@@ -44,185 +41,170 @@
 #include <Magnum/SceneGraph/Scene.h>
 #include <Magnum/Shaders/VertexColor.h>
 
-#include <SDL_mouse.h>
+namespace Magnum { namespace Examples {
 
-namespace Magnum {
-namespace Examples {
+using Object3D = SceneGraph::Object<SceneGraph::MatrixTransformation3D>;
+using Scene3D = SceneGraph::Scene<SceneGraph::MatrixTransformation3D>;
 
-using Object3D =
-    Magnum::SceneGraph::Object<Magnum::SceneGraph::MatrixTransformation3D>;
-using Scene3D =
-    Magnum::SceneGraph::Scene<Magnum::SceneGraph::MatrixTransformation3D>;
+using namespace Math::Literals;
 
-class InteractiveApplication : public Magnum::Platform::Sdl2Application {
-  public:
-    explicit InteractiveApplication(const Arguments &arguments,
-                                    Magnum::NoCreateT)
-        : Magnum::Platform::Sdl2Application{arguments, Magnum::NoCreate} {}
-    explicit InteractiveApplication(const Arguments &arguments,
-                                    const Configuration &conf)
-        : Magnum::Platform::Sdl2Application{arguments, conf} {}
+class MouseInteractionExample: public Platform::Application {
+    public:
+        explicit MouseInteractionExample(const Arguments &arguments);
 
-  protected:
-    float depth_at(int x, int y);
-    Magnum::Vector3 unproject(float x, float y, float z,
-                              bool cam_coords = false);
-    void mousePressEvent(MouseEvent &event) override;
-    void mouseMoveEvent(MouseMoveEvent &event) override;
-    void mouseScrollEvent(MouseScrollEvent &event) override;
+    private:
+        Float depthAt(const Vector2i& position);
+        Vector3 unproject(const Vector2i& position, Float depth);
 
-    Object3D *_camera_object;
-    Magnum::SceneGraph::Camera3D *_camera;
-    float last_z = 0.8;
-    Magnum::Vector3 rp, tp;
+        void mousePressEvent(MouseEvent &event) override;
+        void mouseMoveEvent(MouseMoveEvent &event) override;
+        void mouseScrollEvent(MouseScrollEvent &event) override;
+        void drawEvent() override;
+
+        Shaders::VertexColor3D _shader;
+        GL::Mesh _mesh;
+
+        Scene3D _scene;
+        SceneGraph::DrawableGroup3D _drawables;
+        Object3D* _cameraObject;
+        SceneGraph::Camera3D* _camera;
+
+        Float _lastDepth{0.8f};
+        Vector2i _lastPosition{-1};
+        Vector3 _rotationPoint, _translationPoint;
 };
 
-void InteractiveApplication::mousePressEvent(MouseEvent &event) {
-    int x = event.position().x();
-    int y = event.position().y();
-    float orig_z = depth_at(x, y);
-    float z = orig_z == 1.f ? last_z : orig_z;
-    tp = unproject(x, y, z, /*cam_coords=*/true);
-    if (orig_z != 1.f)
-        rp = tp, last_z = z;
+class Drawable: public SceneGraph::Drawable3D {
+    public:
+        explicit Drawable(Object3D& object, Shaders::VertexColor3D& shader, GL::Mesh& mesh, SceneGraph::DrawableGroup3D& drawables): SceneGraph::Drawable3D{object, &drawables}, _shader(shader), _mesh(mesh) {}
+
+        void draw(const Matrix4& transformation, SceneGraph::Camera3D& camera) {
+            _shader.setTransformationProjectionMatrix(camera.projectionMatrix()*transformation);
+            _mesh.draw(_shader);
+        }
+
+    private:
+        Shaders::VertexColor3D& _shader;
+        GL::Mesh& _mesh;
+};
+
+MouseInteractionExample::MouseInteractionExample(const Arguments &arguments):
+    Platform::Application{arguments, Configuration{}
+        .setTitle("Magnum Mouse Interaction Example")}
+{
+    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+
+    /* Triangle data */
+    const struct {
+        Vector3 pos;
+        Color3 color;
+    } data[]{{{-1.0f, -1.0f, 0.0f}, 0xff0000_rgbf},
+             {{ 1.0f, -1.0f, 0.0f}, 0x00ff00_rgbf},
+             {{ 0.0f,  1.0f, 0.0f}, 0x0000ff_rgbf}};
+
+    /* Triangle mesh */
+    GL::Buffer buffer;
+    buffer.setData(data);
+    _mesh.setCount(3)
+        .addVertexBuffer(std::move(buffer), 0,
+            Shaders::VertexColor3D::Position{},
+            Shaders::VertexColor3D::Color3{});
+
+    /* Triangle object */
+    auto triangle = new Object3D{&_scene};
+    new Drawable{*triangle, _shader, _mesh, _drawables};
+
+    /* Set up the camera */
+    _cameraObject = new Object3D{&_scene};
+    _cameraObject->setTransformation(Matrix4::lookAt(Vector3::zAxis(5), {}, Vector3::yAxis()));
+    _camera = new SceneGraph::Camera3D{*_cameraObject};
+    _camera->setProjectionMatrix(Matrix4::perspectiveProjection(
+        45.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f));
 }
 
-void InteractiveApplication::mouseMoveEvent(MouseMoveEvent &event) {
-    Vector2i pos = event.position();
-    int x = pos.x(), y = pos.y();
-    static Vector2i last_pos = pos;
-    Vector2i delta = pos - last_pos;
-    last_pos = pos;
-    if (event.buttons() & MouseMoveEvent::Button::Left) {
-        Vector3 p = unproject(x, y, last_z, true);
-        _camera_object->translateLocal({tp[0] - p[0], tp[1] - p[1], 0});
-        tp = p;
-        redraw();
-    } else if (event.buttons() & MouseMoveEvent::Button::Right) {
-        float rx = -0.01f * delta.y(), ry = -0.01f * delta.x();
-        Matrix4 t = Matrix4::translation(rp) * Matrix4::rotationX(Rad(rx)) *
-                    Matrix4::rotationY(Rad(ry)) * Matrix4::translation(-rp);
-        _camera_object->transformLocal(t);
-        redraw();
+Float MouseInteractionExample::depthAt(const Vector2i& position) {
+    const Vector2i fbPosition{position.x(), GL::defaultFramebuffer.viewport().sizeY() - position.y() - 1};
+
+    GL::defaultFramebuffer.mapForRead(GL::DefaultFramebuffer::ReadAttachment::Front);
+    Image2D data = GL::defaultFramebuffer.read(
+        Range2Di::fromSize(fbPosition, Vector2i{1}).padded(Vector2i{2}),
+        {GL::PixelFormat::DepthComponent, GL::PixelType::Float});
+
+    return Math::min(Containers::arrayCast<const Float>(data.data()));
+}
+
+Vector3 MouseInteractionExample::unproject(const Vector2i& position, Float depth) {
+    const Range2Di view = GL::defaultFramebuffer.viewport();
+    const Vector2i fbPosition{position.x(), view.sizeY() - position.y() - 1};
+    const Vector3 in{2*Vector2{fbPosition - view.min()}/Vector2{view.size()} - Vector2{1.0f}, depth*2.0f - 1.0f};
+
+    /*
+        Use the following to get global coordinates instead of camera-relative:
+
+        (_cameraObject->absoluteTransformationMatrix()*_camera->projectionMatrix().inverted()).transformPoint(in)
+    */
+    return _camera->projectionMatrix().inverted().transformPoint(in);
+}
+
+void MouseInteractionExample::mousePressEvent(MouseEvent& event) {
+    const Float currentDepth = depthAt(event.position());
+    const Float depth = currentDepth == 1.0f ? _lastDepth : currentDepth;
+    _translationPoint = unproject(event.position(), depth);
+    if(currentDepth != 1.0f) {
+        _rotationPoint = _translationPoint;
+        _lastDepth = depth;
     }
 }
 
-void InteractiveApplication::mouseScrollEvent(MouseScrollEvent &event) {
-    int x, y;
-    SDL_GetMouseState(&x, &y);
-    float orig_z = depth_at(x, y);
-    float z = orig_z == 1.f ? last_z : orig_z;
-    Vector3 p = unproject(x, y, z, /*cam_coords=*/true);
-    if (orig_z != 1.f)
-        rp = p, last_z = z;
-    int dir = event.offset().y();
-    if (!dir)
-        return;
-    Vector3 t = rp * (dir < 0 ? -1 : 1) * 0.1f;
-    // move towards/backwards that point in cam coords
-    _camera_object->translateLocal(t);
+void MouseInteractionExample::mouseMoveEvent(MouseMoveEvent& event) {
+    if(_lastPosition == Vector2i{-1}) _lastPosition = event.position();
+    const Vector2i delta = event.position() - _lastPosition;
+    _lastPosition = event.position();
+
+    if(!event.buttons()) return;
+
+    /* Translate */
+    if(event.modifiers() & MouseMoveEvent::Modifier::Shift) {
+        const Vector3 p = unproject(event.position(), _lastDepth);
+        _cameraObject->translateLocal(_translationPoint - p); /* is Z always 0? */
+        _translationPoint = p;
+
+    /* Rotate around rotation point */
+    } else _cameraObject->transformLocal(
+        Matrix4::translation(_rotationPoint)*
+        Matrix4::rotationX(-0.01_radf*delta.y())*
+        Matrix4::rotationY(-0.01_radf*delta.x())*
+        Matrix4::translation(-_rotationPoint));
+
     redraw();
 }
 
-float InteractiveApplication::depth_at(int x, int y) {
-    Range2Di view = GL::defaultFramebuffer.viewport();
-    y = view.sizeY() - y;
-    GL::defaultFramebuffer.mapForRead(
-        GL::DefaultFramebuffer::ReadAttachment::Front);
-    constexpr int w = 2;
-    Image2D data = GL::defaultFramebuffer.read(
-        Range2Di::fromSize({x - w, y - w}, {2 * w + 1, 2 * w + 1}),
-        {GL::PixelFormat::DepthComponent, GL::PixelType::Float});
-    return *std::min_element(data.data<Float>(),
-                             data.data<float>() + data.pixelSize());
-}
-
-Vector3 InteractiveApplication::unproject(float x, float y, float z,
-                                          bool cam_coords) {
-    Range2Di view = GL::defaultFramebuffer.viewport();
-    y = view.sizeY() - y;
-    Vector4 in = {(x - view.min().x()) / view.sizeX() * 2 - 1,
-                  (y - view.min().y()) / view.sizeY() * 2 - 1, z * 2 - 1, 1};
-    Vector4 out = _camera->projectionMatrix().inverted() * in;
-    if (!cam_coords)
-        out = _camera->cameraMatrix().invertedRigid() * out;
-    out /= out[3];
-    return out.xyz();
-}
-
-template <typename Shader>
-class Drawable : public Object3D, SceneGraph::Drawable3D {
-  public:
-    Drawable(Shader &&shader, Object3D &parent,
-             SceneGraph::DrawableGroup3D &drawables)
-        : Object3D{&parent}, SceneGraph::Drawable3D{*this, &drawables},
-          shader(std::move(shader)) {}
-
-    void draw(const Magnum::Matrix4 &transformation,
-              SceneGraph::Camera3D &camera) {
-        shader.setTransformationProjectionMatrix(camera.projectionMatrix() *
-                                                 transformation);
-        mesh.draw(shader);
+void MouseInteractionExample::mouseScrollEvent(MouseScrollEvent &event) {
+    const Float currentDepth = depthAt(event.position());
+    const Float depth = currentDepth == 1.0f ? _lastDepth : currentDepth;
+    const Vector3 p = unproject(event.position(), depth);
+    if(currentDepth != 1.0f) {
+        _rotationPoint = p;
+        _lastDepth = depth;
     }
 
-    Shader shader;
-    GL::Mesh mesh;
-    GL::Buffer buffer;
-};
+    const Int direction = event.offset().y();
+    if(!direction) return;
 
-class InteractionExample : public InteractiveApplication {
-  public:
-    explicit InteractionExample(const Arguments &arguments);
+    /* Move towards/backwards the rotation point in cam coords */
+    _cameraObject->translateLocal(_rotationPoint*(direction < 0 ? -1.0f : 1.0f)*0.1f);
 
-  private:
-    void drawEvent() override;
-
-    Scene3D _scene;
-    SceneGraph::DrawableGroup3D _drawables;
-};
-
-InteractionExample::InteractionExample(const Arguments &arguments)
-    : InteractiveApplication{arguments, NoCreate} {
-    using namespace Math::Literals;
-
-    create(Configuration{}.setTitle("interaction"),
-           GLConfiguration{}.setSampleCount(4));
-
-    GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
-    GL::Renderer::setClearColor(0x222222_rgbf);
-
-    struct {
-        Vector3 pos;
-        Color3 color;
-    } data[4] = {{{-1.0f, -1.0f, 0.0f}, 0xff0000_rgbf},
-                 {{1.0f, -1.0f, 0.0f}, 0x00ff00_rgbf},
-                 {{0.0f, 1.0f, 0.0f}, 0x0000ff_rgbf}};
-
-    auto tri = new Drawable<Shaders::VertexColor3D>(Shaders::VertexColor3D{},
-                                                    _scene, _drawables);
-    tri->buffer.setData(data);
-    tri->mesh.setPrimitive(GL::MeshPrimitive::TriangleStrip)
-        .setCount(3)
-        .addVertexBuffer(tri->buffer, 0, Shaders::VertexColor3D::Position{},
-                         Shaders::VertexColor3D::Color3{});
-
-    Vector2i view_size = GL::defaultFramebuffer.viewport().size();
-    _camera_object = new Object3D{&_scene};
-    _camera_object->setTransformation(
-        Matrix4::lookAt({0, 0, 5}, {0, 0, 0}, {0, 1, 0}));
-    _camera = new SceneGraph::Camera3D{*_camera_object};
-    _camera->setProjectionMatrix(Matrix4::perspectiveProjection(
-        45.0_degf, Vector2{view_size}.aspectRatio(), 0.01f, 100.0f));
+    redraw();
 }
 
-void InteractionExample::drawEvent() {
-    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color |
-                                 GL::FramebufferClear::Depth);
+void MouseInteractionExample::drawEvent() {
+    GL::defaultFramebuffer.clear(GL::FramebufferClear::Color|GL::FramebufferClear::Depth);
+
     _camera->draw(_drawables);
+
     swapBuffers();
 }
 
-} // namespace Examples
-} // namespace Magnum
+}}
 
-MAGNUM_APPLICATION_MAIN(Magnum::Examples::InteractionExample)
+MAGNUM_APPLICATION_MAIN(Magnum::Examples::MouseInteractionExample)
