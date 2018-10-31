cab50644ce63deb0e64343595c5b2b3d71ec9e20
Vladimír Vondruš <mosra@centrum.cz>
2018-10-31 23:00:19 +0100

    oackage/homebrew: update to 2018.10.

--- package/homebrew/magnum-examples.rb
+++ package/homebrew/magnum-examples.rb
@@ -3,8 +3,8 @@
 class MagnumExamples < Formula
   desc "Examples for the Magnum C++11/C++14 graphics engine"
   homepage "https://github.com/mosra/magnum"
-  url "https://github.com/mosra/magnum-examples/archive/v2018.02.tar.gz"
-  sha256 "b3666c9725d257ab802e2246c15aecad4b3a95eeed74c9a1abea66f9b2aa99ef"
+  url "https://github.com/mosra/magnum-examples/archive/v2018.10.tar.gz"
+  sha256 "d0baac64102f274aedb04dea47434d65ce1ab3338466a629a68bc6ac86facee4"
   head "git://github.com/mosra/magnum-examples.git"
 
   depends_on "cmake"
