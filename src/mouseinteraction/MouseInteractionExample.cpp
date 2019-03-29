d46fb3e719a2802a85ad6dfb10ef3ff3bafcd23e
Vladimír Vondruš <mosra@centrum.cz>
2019-03-29 09:43:52 +0100

    mouseinteraction: fix build without deprecated APIs.

--- src/mouseinteraction/MouseInteractionExample.cpp
+++ src/mouseinteraction/MouseInteractionExample.cpp
@@ -34,6 +34,7 @@
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/GL/PixelFormat.h>
 #include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/FunctionsBatch.h>
 #include <Magnum/MeshTools/Compile.h>
 #include <Magnum/Platform/Sdl2Application.h>
 #include <Magnum/Primitives/Grid.h>
