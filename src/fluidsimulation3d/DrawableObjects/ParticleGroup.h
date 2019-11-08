b6cd69604473b0799d7e4de56f18520709436659
Vladimír Vondruš <mosra@centrum.cz>
2019-11-08 15:59:52 +0100

    fluidsimulation3d: don't return mutable references from getters.

--- src/fluidsimulation3d/DrawableObjects/ParticleGroup.h
+++ src/fluidsimulation3d/DrawableObjects/ParticleGroup.h
@@ -47,56 +47,56 @@ class ParticleGroup {
 
         ParticleGroup& draw(Containers::Pointer<SceneGraph::Camera3D>& camera, const Vector2i& viewportSize);
 
-        bool& isDirty() { return _dirty; }
+        bool isDirty() const { return _dirty; }
 
         ParticleGroup& setDirty() {
             _dirty = true;
             return *this;
         }
 
-        Float& particleRadius() { return _particleRadius; }
+        Float particleRadius() const { return _particleRadius; }
 
         ParticleGroup& setParticleRadius(Float radius) {
             _particleRadius = radius;
             return *this;
         }
 
-        ParticleSphereShader::ColorMode& colorMode() { return _colorMode; }
+        ParticleSphereShader::ColorMode colorMode() const { return _colorMode; }
 
         ParticleGroup& setColorMode(ParticleSphereShader::ColorMode colorMode) {
             _colorMode = colorMode;
             return *this;
         }
 
-        Color3& ambientColor() { return _ambientColor; }
+        Color3 ambientColor() const { return _ambientColor; }
 
         ParticleGroup& setAmbientColor(const Color3& color) {
             _ambientColor = color;
             return *this;
         }
 
-        Color3& diffuseColor() { return _diffuseColor; }
+        Color3 diffuseColor() const { return _diffuseColor; }
 
         ParticleGroup& setDiffuseColor(const Color3& color) {
             _diffuseColor = color;
             return *this;
         }
 
-        Color3& specularColor() { return _specularColor; }
+        Color3 specularColor() const { return _specularColor; }
 
         ParticleGroup& setSpecularColor(const Color3& color) {
             _specularColor = color;
             return *this;
         }
 
-        Float& shininess() { return _shininess; }
+        Float shininess() const { return _shininess; }
 
         ParticleGroup& setShininess(Float shininess) {
             _shininess = shininess;
             return *this;
         }
 
-        Vector3& lightDirection() { return _lightDir; }
+        Vector3 lightDirection() const { return _lightDir; }
 
         ParticleGroup& setLightDirection(const Vector3& lightDir) {
             _lightDir = lightDir;
