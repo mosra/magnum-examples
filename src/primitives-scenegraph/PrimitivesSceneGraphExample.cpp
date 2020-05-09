3aad33707a43d8889979ef13c26b1829137af619
Brandon Pomeroy <pomeroyb@users.noreply.github.com>
2020-05-09 03:23:52 -0700

    Update author information

--- src/primitives-scenegraph/PrimitivesSceneGraphExample.cpp
+++ src/primitives-scenegraph/PrimitivesSceneGraphExample.cpp
@@ -3,8 +3,9 @@
 
     Original authors — credit is appreciated but not required:
 
-        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 —
+        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 —
             Vladimír Vondruš <mosra@centrum.cz>
+			Brandon Pomeroy <brandon@pomeroyb.com>
 
     This is free and unencumbered software released into the public domain.
 
