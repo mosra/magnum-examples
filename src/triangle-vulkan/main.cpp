ff0a1e5ddab31a32746d9fb40604c995faec533d
Vladimír Vondruš <mosra@centrum.cz>
2021-02-06 12:03:27 +0100

    triangle-vulkan: use the new pipeline bind command.

--- src/triangle-vulkan/main.cpp
+++ src/triangle-vulkan/main.cpp
@@ -243,7 +243,7 @@ int main(int argc, char** argv) {
         );
 
     /* Bind the pipeline */
-    vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+    cmd.bindPipeline(pipeline);
 
     /* Bind the vertex buffer */
     {
