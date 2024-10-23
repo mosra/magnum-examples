8b9f55ef29c572a1b93c7818479cb3803ebe160a
Vladimír Vondruš <mosra@centrum.cz>
2024-10-23 11:40:28 +0200

    audio: adapt to Magnum key event changes.

--- src/audio/AudioExample.cpp
+++ src/audio/AudioExample.cpp
@@ -203,17 +203,18 @@ void AudioExample::drawEvent() {
 }
 
 void AudioExample::keyPressEvent(KeyEvent& event) {
-    if(event.key() == KeyEvent::Key::Down)
+    if(event.key() == Key::Down)
         _sourceRig.rotateXLocal(-5.0_degf);
-    else if(event.key() == KeyEvent::Key::Up)
+    else if(event.key() == Key::Up)
         _sourceRig.rotateXLocal(5.0_degf);
-    else if(event.key() == KeyEvent::Key::Left)
+    else if(event.key() == Key::Left)
         _sourceRig.rotateY(5.0_degf);
-    else if(event.key() == KeyEvent::Key::Right)
+    else if(event.key() == Key::Right)
         _sourceRig.rotateY(-5.0_degf);
-    else if(event.key() == KeyEvent::Key::PageUp)
+    else if(event.key() == Key::PageUp)
         _sourceObject.translate(Vector3::zAxis(-0.25f));
-    else if(event.key() == KeyEvent::Key::PageDown && _sourceObject.transformation().translation().z() < 0.0f)
+    else if(event.key() == Key::PageDown &&
+            _sourceObject.transformation().translation().z() < 0.0f)
         _sourceObject.translate(Vector3::zAxis(0.25f));
     else return;
 
