6b022fa0764cf4b063aa10eae84aead69d806270
Vladimír Vondruš <mosra@centrum.cz>
2025-04-18 13:41:49 +0200

    package/homebrew: make the 2020.06 version still work with CMake 4.

--- package/homebrew/magnum-examples.rb
+++ package/homebrew/magnum-examples.rb
@@ -22,6 +22,11 @@ class MagnumExamples < Formula
     # 2020.06 has the options unprefixed, current master has them prefixed.
     # Options not present in 2020.06 are prefixed always.
     option_prefix = build.head? ? 'MAGNUM_' : ''
+    # 2020.06 has CMake 3.5 as minimum required for backwards compatibility
+    # purposes, but it works with any newer. CMake 4.0 removed compatibility
+    # with it and suggests this as an override.
+    # TODO remove once a new release is finally made
+    extra_cmake_args = build.head? ? [] : ['-DCMAKE_POLICY_VERSION_MINIMUM=3.5']
 
     system "mkdir build"
     cd "build" do
@@ -29,7 +34,7 @@ class MagnumExamples < Formula
       # https://github.com/Homebrew/homebrew-core/pull/4482 and nothing
       # happened with https://github.com/erincatto/Box2D/issues/431 yet
       system "cmake",
-      *std_cmake_args,
+        *(std_cmake_args + extra_cmake_args),
         # Without this, ARM builds will try to look for dependencies in
         # /usr/local/lib and /usr/lib (which are the default locations) instead
         # of /opt/homebrew/lib which is dedicated for ARM binaries. Please
